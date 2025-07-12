a834e4794d3e8c791ad185eafa5f572c84a6d68c
Erik Aylward <y_fantasy_gem@proton.me>
2025-07-12 10:45:38 -0400

    Setup zeitwerk

--- lib/y_fantasy.rb
+++ lib/y_fantasy.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "y_fantasy/version"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module YFantasy
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/y_fantasy_spec.rb
+++ spec/y_fantasy_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe YFantasy do
   it "has a version number" do
     expect(YFantasy::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
